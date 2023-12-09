5919ac9127424fe91d64d0182e8250c12154ebd7
Aryasharii <105221013@student.universitaspertamina.ac.id>
2023-12-09 21:50:51 +0700

    tht Praktikum Pemweb 8

---
+++ TAKE HOME TASK/login.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $validUsername = "arya@gmail.com";
+    $validPassword = "password123"; 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Validasi login
+    if ($username === $validUsername && $password === $validPassword) {
+
+        header("Location: index.html");
+        exit;
+    } else {
+        
+        header("Location: login.html?error=1");
+        exit;
+    }
+} else {
+    header("Location: login.html");
+    exit;
+}
+?>
